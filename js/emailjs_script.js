3a3ca6ca6360b0331adca7f3748f80bfb80de796
stamenkovicsstefan <stamenkovicsstefan@gmail.com>
2025-04-25 17:10:34 +0200

    EmailJS edits, text edits

--- js/emailjs_script.js
+++ js/emailjs_script.js
@@ -6,17 +6,20 @@ function sendMail(){
         message : document.getElementById("textId54").value
     }
     
-    /*
     const emailValue = document.getElementById("textId52").value;
     const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    if (!emailPattern.test(emailValue)) {
-        alert('Please enter a valid email address.');
-        return false;
-    }
-    else {
-        emailjs.send("service_jb8kvvr", "template_e5pl0q7", parms).then(alert("Sent!"));
+    
+    emailjs.send("service_jb8kvvr", "template_e5pl0q7", parms)
+    alert("Vaša poruka je poslata! Javićemo vam se u najkraćem roku.\n\nYour message is sent! We'll contact you soon.");
+    
+    document.getElementById("textId53").value = "";
+    document.getElementById("textId52").value = "";
+    document.getElementById("textId51").value = "";
+    document.getElementById("textId54").value = "";
+    /*
+    if (emailPattern.test(emailValue)) {
+        emailjs.send("service_jb8kvvr", "template_e5pl0q7", parms).then(alert("Vaša poruka je poslata! Javićemo vam se u najkraćem roku.\n\nYour message is sent! We'll contact you soon."));
+        //return;
     }
     */
-
-    emailjs.send("service_jb8kvvr", "template_e5pl0q7", parms).then(alert("Sent!"));
 }
