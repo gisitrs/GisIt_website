bb0054707bc114773948db336bc0bcdd7587d8de
igor94grozdanic <igor94grozdanic@gmail.com>
2025-04-27 15:16:38 +0200

    ReCaptcha added

--- js/emailjs_script.js
+++ js/emailjs_script.js
@@ -1,9 +1,17 @@
 function sendMail(){
+    // Check if reCAPTCHA is completed
+    const recaptchaResponse = grecaptcha.getResponse();
+    if (!recaptchaResponse) {
+        alert("Please complete the reCAPTCHA.");
+        return;
+    }
+
     let parms = {
         subject : document.getElementById("textId53").value,
         email : document.getElementById("textId52").value,
         name : document.getElementById("textId51").value,
-        message : document.getElementById("textId54").value
+        message : document.getElementById("textId54").value,
+        "g-recaptcha-response": recaptchaResponse // Include reCAPTCHA response
     }
     
     const emailValue = document.getElementById("textId52").value;
@@ -12,10 +20,12 @@ function sendMail(){
     emailjs.send("service_jb8kvvr", "template_e5pl0q7", parms)
     alert("Vaša poruka je poslata! Javićemo vam se u najkraćem roku.\n\nYour message is sent! We'll contact you soon.");
     
-    document.getElementById("textId53").value = "";
-    document.getElementById("textId52").value = "";
-    document.getElementById("textId51").value = "";
-    document.getElementById("textId54").value = "";
+    grecaptcha.reset(); // Reset reCAPTCHA
+    document.getElementById("textId53").value;
+    document.getElementById("textId52").value;
+    document.getElementById("textId51").value;
+    document.getElementById("textId54").value;
+
     /*
     if (emailPattern.test(emailValue)) {
         emailjs.send("service_jb8kvvr", "template_e5pl0q7", parms).then(alert("Vaša poruka je poslata! Javićemo vam se u najkraćem roku.\n\nYour message is sent! We'll contact you soon."));
